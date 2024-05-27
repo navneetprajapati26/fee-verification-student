5b01ed986b2e6c8f58401eadc2219010d8e5a357
Navneet Prajapati <navneetprajapati26@gmail.com>
2024-05-27 19:46:37 +0530

    FeeReceipt upload done by Navneet

--- lib/features/fee_risipts/screen/all_fee_risipts.dart
+++ lib/features/fee_risipts/screen/all_fee_risipts.dart
@@ -26,35 +26,6 @@ class _AllFeeReceiptState extends State<AllFeeReceipt> {
       appBar: AppBar(
         title: const Text('All Fee receipts'),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              context.read<FeeReceiptBloc>().add(UploadImageEvent(
-                  folderName: 'your_folder_name',
-                  receiptName: 'your_receipt_name',
-                  receiptYear: 'your_receipt_year',
-                  receiptAmount: 'your_receipt_amount'));
-            },
-            child: const Icon(Icons.cloud_upload),
-            heroTag: 'imageUpload',
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              context
-                  .read<FeeReceiptBloc>()
-                  .add(UploadPDFEvent( folderName: 'your_folder_name',
-                  receiptName: 'your_receipt_name',
-                  receiptYear: 'your_receipt_year',
-                  receiptAmount: 'your_receipt_amount'));
-            },
-            child: const Icon(Icons.picture_as_pdf),
-            heroTag: 'pdfUpload',
-          ),
-        ],
-      ),
 
       body: BlocBuilder<FeeReceiptBloc, FeeReceiptState>(
         builder: (context, state) {
@@ -82,34 +53,119 @@ class _AllFeeReceiptState extends State<AllFeeReceipt> {
             child: Text('All Fee receipts'),
           );
         },
-      )
+      ),
+      floatingActionButton: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            context.read<FeeReceiptBloc>().add(UploadImageEvent(
+                folderName: 'your_folder_name',
+                receiptName: 'your_receipt_name',
+                receiptYear: 'your_receipt_year',
+                receiptAmount: 'your_receipt_amount'));
 
-      // body: BlocBuilder<FeeReceiptBloc, FeeReceiptState>(
-      //   builder: (context, state) {
-      //     if (state.postStatus == FeeReceiptPostStatus.loading) {
-      //       return const Center(
-      //         child: CircularProgressIndicator(),
-      //       );
-      //     }
-      //     if (state.postStatus == FeeReceiptPostStatus.imageLoaded) {
-      //       return Center(
-      //         child: state.feeReceiptModel?.receiptUrl != null
-      //             ? Image.network(state.feeReceiptModel?.receiptUrl ?? '')
-      //             : Text('No image uploaded'),
-      //       );
-      //     }
-      //     if (state.postStatus == FeeReceiptPostStatus.pdfLoaded) {
-      //       return Center(
-      //         child: state.feeReceiptModel?.receiptUrl != null
-      //             ? ViewPDF(url: state.feeReceiptModel?.receiptUrl ?? '')
-      //             : Text('No PDF uploaded'),
-      //       );
-      //     }
-      //     return const Center(
-      //       child: Text('All Fee receipts'),
-      //     );
-      //   },
-      // ),
+            //_addNewReceipt(context);
+          },
+          child: const Icon(Icons.cloud_upload),
+          heroTag: 'imageUpload',
+        ),
+        SizedBox(height: 10),
+        FloatingActionButton(
+          onPressed: () {
+            context
+                .read<FeeReceiptBloc>()
+                .add(UploadPDFEvent( folderName: 'your_folder_name',
+                receiptName: 'your_receipt_name',
+                receiptYear: 'your_receipt_year',
+                receiptAmount: 'your_receipt_amount'));
+          },
+          child: const Icon(Icons.picture_as_pdf),
+          heroTag: 'pdfUpload',
+        ),
+      ],
+    ),
+
     );
   }
+
+  Widget _addNewReceipt(BuildContext context) {
+
+    String? receiptName = "";
+    String? receiptYear = "";
+    String? receiptAmount = "";
+
+
+    final _formKey = GlobalKey<FormState>();
+    return Dialog(
+      surfaceTintColor: Colors.white,
+      insetPadding: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+      child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Receipt Name'),
+                      validator: (value) => value!.isEmpty
+                          ? 'Please enter a receiptName'
+                          : null,
+                      onSaved: (value) => receiptName = value!,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Receipt Year'),
+                      validator: (value) => value!.isEmpty
+                          ? 'Please enter a Year'
+                          : null,
+                      onSaved: (value) => receiptYear = value,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Receipt Amount'),
+                      validator: (value) => value!.isEmpty
+                          ? 'Please enter a receipt amount'
+                          : null,
+                      onSaved: (value) => receiptAmount = value,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  TextButton(
+                    child: const Text('Add'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        context
+                            .read<FeeReceiptBloc>()
+                            .add(UploadPDFEvent( folderName: 'your_folder_name',
+                            receiptName: receiptName!,
+                            receiptYear: receiptYear!,
+                            receiptAmount: receiptAmount!));
+
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+
 }
